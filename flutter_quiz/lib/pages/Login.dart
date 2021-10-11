3f426b946b5760de7284456f09db069eb02c94fb
JCadiz2000 <cadizjayson25@yahoo.com>
2021-10-11 13:50:04 +0800

    Added Login

--- flutter_quiz/lib/pages/Login.dart
+++ flutter_quiz/lib/pages/Login.dart
@@ -9,30 +9,132 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  buildUsernameInput() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Email',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16.0,
+          ),
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60.0,
+          child: TextField(
+            keyboardType: TextInputType.name,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide(color: Colors.black, width: 1.0)),
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.black,
+              ),
+              hintText: 'Enter your email',
+              hintStyle: TextStyle(color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  buildPasswordInput() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Password',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16.0,
+          ),
+        ),
+        SizedBox(height: 10.0),
+        Container(
+          alignment: Alignment.centerLeft,
+          height: 60.0,
+          child: TextField(
+            obscureText: true,
+            keyboardType: TextInputType.name,
+            style: TextStyle(color: Colors.black),
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                  borderSide: BorderSide(color: Colors.black, width: 1.0)),
+              contentPadding: EdgeInsets.only(top: 14.0),
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.black,
+              ),
+              hintText: 'Enter your password',
+              hintStyle: TextStyle(color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  buildLoginBtn() {
+    return ElevatedButton.icon(
+      onPressed: () {
+        login(context);
+      },
+      label: Text("Login"),
+      icon: Icon(Icons.login),
+      style: ElevatedButton.styleFrom(fixedSize: Size(100, 50)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                "Login Page",
-                style: TextStyle(fontSize: 20.0),
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                physics: AlwaysScrollableScrollPhysics(),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 40.0,
+                  vertical: 120.0,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Login-Page',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 35.0,
+                    ),
+                    buildUsernameInput(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    buildPasswordInput(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    buildLoginBtn(),
+                  ],
+                ),
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton.icon(
-                onPressed: () {
-                  login(context);
-                },
-                label: Text("Login"),
-                icon: Icon(Icons.login),
-                style: ElevatedButton.styleFrom(fixedSize: Size(100, 50)),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
